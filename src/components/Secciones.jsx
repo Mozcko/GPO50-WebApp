4d426f4fb639c6bed83f3b3cef1f3b22743eb7d9
Mozcko <musquiz02@gmail.com>
2023-08-27 09:45:24 -0600

    cambios en hero y comienzo de secciones

--- src/components/Secciones.jsx
+++ src/components/Secciones.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { secciones } from "../constants";
+import { fadeIn } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const Card = ({ index, title, logo, page }) => {
+  return (
+    <Tilt>
+      <motion.div variants={fadeIn("right", "spring", index * 0.5, 0.75)}>
+        test
+      </motion.div>
+    </Tilt>
+  );
+};
 
 export function Secciones() {
   return (
-    <div>Secciones</div>
-  )
+    <>
+      <div className="mt-10 flex flex-wrap gap-10">
+        {secciones.map((seccion, index) => (
+          <Card key={seccion.title} index={index} {...seccion} />
+        ))}
+      </div>
+    </>
+  );
 }
+
+
+export default SectionWrapper(Secciones, "")
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
-const styles = {};
+const styles = {
+    padding: "sm:px-16 px-6 sm:py-16 py-10",
+};
 
 export { styles };
